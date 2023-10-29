78638a2c9d646ed9ee6e86b988a3f3ae4f0413bf
Murilo Vieira <murilo12super@gmail.com>
2023-10-29 13:33:24 -0300

    Inicio do projeto

---
+++ src/main/java/com/senai/sistema_rh_sa/SistemaRhSaApplication.java
@@ -0,0 +1,13 @@
+package com.senai.sistema_rh_sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRhSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRhSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/sistema_rh_sa/SistemaRhSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.sistema_rh_sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRhSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

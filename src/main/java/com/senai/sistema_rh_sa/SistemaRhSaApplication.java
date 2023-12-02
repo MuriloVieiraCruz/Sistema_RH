58d0a882b30623a080f42f450a239ddc6d247e12
Murilo Vieira <murilo12super@gmail.com>
2023-12-02 12:43:38 -0300

    Gerador de repasses para testes

--- src/main/java/com/senai/sistema_rh_sa/SistemaRhSaApplication.java
+++ src/main/java/com/senai/sistema_rh_sa/SistemaRhSaApplication.java
@@ -33,8 +33,48 @@ public class SistemaRhSaApplication {
         return args -> {
             System.out.println("The system started");
 
+            List<Frete> fretesRealizados = new ArrayList<>();
 
-            
+            Random random = new Random();
+
+            for (int i = 0; i < 40; i++) {
+                BigDecimal valorTotal = BigDecimal.valueOf(random.nextDouble() * 600).setScale(2, RoundingMode.CEILING);
+                int idEntregador = random.nextInt(5) + 1;
+
+                Frete frete = new Frete(i + 1, valorTotal, Instant.now(), idEntregador);
+                fretesRealizados.add(frete);
+            }
+
+            Map<Integer, BigDecimal> totalPorEntregador = new HashMap<>();
+
+            for (Frete frete : fretesRealizados) {
+                int idEntregador = frete.getIdEntregador();
+                BigDecimal valorTotal = frete.getValorTotal();
+
+                if (totalPorEntregador.containsKey(idEntregador)) {
+                    BigDecimal totalAtual = totalPorEntregador.get(idEntregador);
+                    totalPorEntregador.put(idEntregador, totalAtual.add(valorTotal));
+                } else {
+                    totalPorEntregador.put(idEntregador, valorTotal);
+                }
+            }
+
+            System.out.println("Total por Entregador:");
+            for (Map.Entry<Integer, BigDecimal> entry : totalPorEntregador.entrySet()) {
+                System.out.println("Entregador " + entry.getKey() + ": R$" + entry.getValue());
+            }
+
+
+            List<Repasse> repasses = service.calcularRepassesPor(fretesRealizados, 2023, 11);
+
+            for (Repasse repasse : repasses) {
+                System.out.println("ID - " + repasse.getId() + " - ");
+                System.out.println("valorBruto - " + repasse.getValorBruto() + " - ");
+                System.out.println("valorLiquido - " + repasse.getValorLiquido() + " - ");
+                System.out.println("taxaDeSeguro - " + repasse.getTaxaSeguroDeVida() + " - ");
+                System.out.println("bonificacao - " + repasse.getBonificacao() + " - ");
+                System.out.println("--------------------------------------");
+            }
         };
     }
 
